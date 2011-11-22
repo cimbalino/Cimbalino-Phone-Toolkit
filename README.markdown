2ac8e26d29417cca02c6cff0a72622c570424089
Pedro Lamas <pedrolamas@gmail.com>
2011-11-22 14:50:47 +0000

    Updated the build script and readme file

--- README.markdown
+++ README.markdown
@@ -13,9 +13,24 @@ The Toolkit is divided in projects, regarding different Windows Phone applicatio
 * __Cimbalino.Phone.Toolkit.Camera__ - MVVM compatible services for camera access
 * __Cimbalino.Phone.Toolkit.PhoneDialer__ - MVVM compatible services for phone dialer access
 
-__Note:__ this toolkit is only compatible with the [Windows Phone SDK 7.1][4]!
+> **Note:** this toolkit is only compatible with the [Windows Phone SDK 7.1][4]!
+
+## Nuget
+
+The packages are available thru [NuGet][7], with the same handles as the project names (e.g., the main package is "Cimbalino.Phone.Toolkit")
+
+## Download
+
+Please visit the [download section on GitHub][6] to download the latest release of the toolkit.
+
+## License
+
+See the [LICENSE.txt][5] file for details.
 
 [1]: http://www.pedrolamas.com
 [2]: http://twitter.com/pedrolamas
 [3]: https://github.com/PedroLamas/Cimbalino-Phone-Toolkit/raw/master/Cimbalino.Phone.Toolkit.png "Cimbalino Windows Phone Toolkit"
 [4]: http://go.microsoft.com/?linkid=9772716
+[5]: https://github.com/PedroLamas/Cimbalino-Phone-Toolkit/raw/master/LICENSE.txt "Cimbalino Windows Phone Toolkit License"
+[6]: https://github.com/PedroLamas/Cimbalino-Phone-Toolkit/downloads
+[7]: http://nuget.org
--- build/default.ps1
+++ build/default.ps1
@@ -3,49 +3,116 @@ properties {
   $buildDir = "$baseDir\build"
   $sourceDir = "$baseDir\src"
   $toolsDir = "$baseDir\tools"
-  $releaseDir = "$baseDir\bin\release"
-  $workingDir = "$baseDir\bin\working"
+  $binDir = "$baseDir\bin"
+  
+  $assemblyVersion = "1.0.0.0"
+  $fileVersion = $assemblyVersion
+  
+  $tempDir = "$binDir\temp"
+  $binariesDir = "$binDir\binaries"
+  $zipDir = "$binDir\zip"
+  $nupkgDir = "$binDir\nupkg"
   
   $nuget = "$toolsDir\nuget\nuget.exe"
+  $7zip = "$toolsDir\7zip\7za.exe"
   
-  $builds = @(
+  $projects = @(
     @{Name = "Cimbalino.Phone.Toolkit"},
     @{Name = "Cimbalino.Phone.Toolkit.Camera"},
+    #@{Name = "Cimbalino.Phone.Toolkit.Location"},
     @{Name = "Cimbalino.Phone.Toolkit.PhoneDialer"}
   )
 }
 
 $framework = '4.0x86'
 
-task default -depends Package
+task default -depends Help
 
-task Clean {
-  Set-Location $baseDir
-  
-  if (Test-Path -path $workingDir)
+task Clean -description "Clean the output folder" {
+  if (Test-Path -path $binDir)
   {
-    Write-Output "Deleting Working Directory"
+    Write-Host -ForegroundColor Green "Deleting Working Directory"
+    Write-Host
     
-    Remove-Item $workingDir -Recurse -Force
+    Remove-Item $binDir -Recurse -Force
   }
   
-  New-Item -Path $workingDir -ItemType Directory | Out-Null
+  New-Item -Path $binDir -ItemType Directory | Out-Null
 }
 
-task Package -depends Clean {
-  $builds | % {
+task Version -description "Updates the version entries in AssemblyInfo.cs files" {
+  $assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+  $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+  $assemblyVersionValue = 'AssemblyVersion("' + $assemblyVersion + '")';
+  $fileVersionValue = 'AssemblyFileVersion("' + $fileVersion + '")';
+  
+  $projects | % {
+    $name = $_.Name
+    $projectDir = "$sourceDir\$name\"
+    
+    Write-Host -ForegroundColor Green "Versioning $name..."
+    Write-Host
+    
+    Get-ChildItem -Path $projectDir -Filter AssemblyInfo.cs -Recurse | % {
+      $filename = $_.FullName
+      
+      (Get-Content -Path $filename) | % {
+        % { $_ -Replace $assemblyVersionPattern, $assemblyVersionValue } |
+        % { $_ -Replace $fileVersionPattern, $fileVersionValue }
+      } | Set-Content -Path $filename -Encoding UTF8
+    }
+  }
+}
+
+task Build -depends Clean, Version -description "Build all projects and get the assemblies" {
+  $tempBinariesDir = "$tempDir\binaries"
+  
+  New-Item -Path $binariesDir -ItemType Directory | Out-Null
+  New-Item -Path $tempBinariesDir -ItemType Directory | Out-Null
+  
+  $projects | % {
+    $name = $_.Name
+    $csproj = "$sourceDir\$name\$name.csproj"
+
+    Write-Host -ForegroundColor Green "Building $name..."
+    Write-Host
+    
+    Exec { msbuild "/t:Clean;Build" /p:Configuration=Release /p:OutputPath=$tempBinariesDir $csproj } "Error building $name"
+    
+    Copy-Item -Path "$tempBinariesDir\*.*" -Destination "$binariesDir\" -Recurse
+  }
+}
+
+task PackZip -depends Build -description "Create a zip file with the resulting assemblies" {
+  $tempZipDir = "$tempDir\zip"
+  
+  New-Item -Path $zipDir -ItemType Directory | Out-Null
+  New-Item -Path $tempZipDir -ItemType Directory | Out-Null
+  
+  Copy-Item -Path "$binariesDir\*.*" -Destination "$tempZipDir\" -Recurse
+  Copy-Item -Path "$sourceDir\License.txt" -Destination "$tempZipDir\" -Recurse
+  
+  $zipVersion = $assemblyVersion -replace "(\d+.\d+).*", '$1'
+  
+  Exec { .$7zip a -tzip "$zipDir\Cimbalino.Phone.Toolkit.$zipVersion.zip" "$tempZipDir\*.*" } "Error packaging $name"
+}
+
+task PackNuGet -depends Clean, Version -description "Create the NuGet packages" {
+  New-Item -Path $nupkgDir -ItemType Directory | Out-Null
+  
+  $projects | % {
     $name = $_.Name
     $csproj = "$sourceDir\$name\$name.csproj"
 
     Write-Host -ForegroundColor Green "Packaging $name..."
     Write-Host
     
-    Exec { .$nuget pack $csproj -Prop Configuration=Release -Build -OutputDirectory $workingDir } "Error packaging $name"
+    Exec { .$nuget pack $csproj -Prop Configuration=Release -Build -OutputDirectory $nupkgDir } "Error packaging $name"
   }
 }
 
-task Publish -depends Package {
-  Get-ChildItem $workingDir\*.nupkg | % {
+task PublishNuget -depends Package -description "Publish the NuGet packages to the remote repositories" {
+  Get-ChildItem $nupkgDir\*.nupkg | % {
     $nupkg = $_.FullName
     
     Write-Host -ForegroundColor Green "Publishing $nupkg..."
@@ -53,4 +120,8 @@ task Publish -depends Package {
     
     Exec { .$nuget push $nupkg } "Error publishing $nupkg"
   }
+}
+
+task Help -description "Show the help screen" {
+  Write-Documentation
 }
