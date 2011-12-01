2637b35610ef5ff54d54f4eefa615c04836eec9a
Pedro Lamas <pedrolamas@gmail.com>
2011-12-01 13:02:50 +0000

    Implemented a more generic ICollectionExtensions than the previous ArrayExtensions

--- src/Cimbalino.Phone.Toolkit/Extensions/ICollectionExtensions.cs
+++ src/Cimbalino.Phone.Toolkit/Extensions/ICollectionExtensions.cs
@@ -1,11 +1,11 @@
 ﻿// ****************************************************************************
-// <copyright file="ArrayExtensions.cs" company="Pedro Lamas">
+// <copyright file="ICollectionExtensions.cs" company="Pedro Lamas">
 // Copyright © Pedro Lamas 2011
 // </copyright>
 // ****************************************************************************
 // <author>Pedro Lamas</author>
 // <email>pedrolamas@gmail.com</email>
-// <date>17-11-2011</date>
+// <date>01-12-2011</date>
 // <project>Cimbalino.Phone.Toolkit</project>
 // <web>http://www.pedrolamas.com</web>
 // <license>
@@ -13,22 +13,24 @@
 // </license>
 // ****************************************************************************
 
+using System.Collections.Generic;
+
 namespace Cimbalino.Phone.Toolkit.Extensions
 {
     /// <summary>
-    /// Provides a set of static (Shared in Visual Basic) methods for <see cref="System.Array"/> instances
+    /// Provides a set of static (Shared in Visual Basic) methods for <see cref="ICollection"/> instances
     /// </summary>
-    public static class ArrayExtensions
+    public static class ICollectionExtensions
     {
         /// <summary>
-        /// Determines whether the specified array is null or empty.
+        /// Determines whether the specified <see cref="ICollection{TSource}"/> is null or empty.
         /// </summary>
-        /// <param name="array">The array to check.</param>
-        /// <typeparam name="TSource">The type of the array to check.</typeparam>
-        /// <returns>True if the array is null or empty; otherwise, false.</returns>
-        public static bool IsNullOrEmpty<TSource>(this TSource[] array)
+        /// <param name="collection">The <see cref="ICollection{TSource}"/> to check.</param>
+        /// <typeparam name="TSource">The <see cref="ICollection"/> type to check.</typeparam>
+        /// <returns>True if the collection is null or empty; otherwise, false.</returns>
+        public static bool IsNullOrEmpty<TSource>(this ICollection<TSource> collection)
         {
-            return array == null || array.Length == 0;
+            return collection == null || collection.Count == 0;
         }
     }
 }
