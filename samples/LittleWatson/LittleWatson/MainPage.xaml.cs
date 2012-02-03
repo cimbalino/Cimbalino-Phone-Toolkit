2a2c84cba0ed1f7648b8c4a5b3eb4b1e6601f969
Pedro Lamas <pedrolamas@gmail.com>
2012-02-03 10:02:16 +0000

    Added the LittleWatson sample

---
+++ samples/LittleWatson/LittleWatson/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Phone.Controls;
+
+namespace LittleWatson
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Cimbalino.Phone.Toolkit.Helpers.LittleWatson.CheckForPreviousException();
+        }
+
+        private void ThrowException_Click(object sender, RoutedEventArgs e)
+        {
+            //If you run this code from Visual Studio, it will stop here; just press "Continue" when that happens
+            throw new Exception("My test exception");
+        }
+    }
+}
