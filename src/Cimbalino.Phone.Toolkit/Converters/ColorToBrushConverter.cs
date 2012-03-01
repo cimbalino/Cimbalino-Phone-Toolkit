42aaa33674293d72c5996baa03dcad133a2afd17
Pedro Lamas <pedrolamas@gmail.com>
2012-03-01 12:32:40 +0000

    Added the ColorToBrushConverter class

---
+++ src/Cimbalino.Phone.Toolkit/Converters/ColorToBrushConverter.cs
@@ -0,0 +1,69 @@
+﻿// ****************************************************************************
+// <copyright file="ColorToBrushConverter.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2011
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>01-03-2012</date>
+// <project>Cimbalino.Phone.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Cimbalino.Phone.Toolkit.Converters
+{
+    /// <summary>
+    /// An <see cref="IValueConverter"/> which converts a <see cref="Color"/> value to a <see cref="SolidColorBrush"/> value.
+    /// </summary>
+    public class ColorToBrushConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a <see cref="Color"/> value to a <see cref="SolidColorBrush"/> value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null || !(value is Color))
+            {
+                return null;
+            }
+
+            return new SolidColorBrush((Color)value);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="SolidColorBrush"/> value to a <see cref="Color"/> value.
+        /// </summary>
+        /// <param name="value">The value produced by the binding source.</param>
+        /// <param name="targetType">The type of the binding target property.</param>
+        /// <param name="parameter">The converter parameter to use.</param>
+        /// <param name="culture">The culture to use in the converter.</param>
+        /// <returns>
+        /// A converted value. If the method returns null, the valid null value is used.
+        /// </returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var brush = value as SolidColorBrush;
+
+            if (brush == null)
+            {
+                return null;
+            }
+
+            return brush.Color;
+        }
+    }
+}
