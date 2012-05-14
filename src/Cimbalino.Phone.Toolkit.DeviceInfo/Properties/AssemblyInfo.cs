127815932ce24f6ef57f6c92601f98d2a7850e5e
Pedro Lamas <pedrolamas@gmail.com>
2012-05-14 12:06:02 +0100

    Cleaned up the AssemblyInfo.cs files from all projects

--- src/Cimbalino.Phone.Toolkit.DeviceInfo/Properties/AssemblyInfo.cs
+++ src/Cimbalino.Phone.Toolkit.DeviceInfo/Properties/AssemblyInfo.cs
@@ -18,9 +18,6 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Cimbalino.Phone.Toolkit.DeviceInfo")]
 [assembly: AssemblyDescription("Cimbalino Windows Phone Toolkit")]
 [assembly: AssemblyConfiguration("")]
@@ -30,23 +27,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("070c4866-d5b4-4850-867a-75efaccea4e9")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.2.2.0")]
 [assembly: AssemblyFileVersion("1.2.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
--- src/Cimbalino.Phone.Toolkit.UserInfo/Properties/AssemblyInfo.cs
+++ src/Cimbalino.Phone.Toolkit.UserInfo/Properties/AssemblyInfo.cs
@@ -18,9 +18,6 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Cimbalino.Phone.Toolkit.UserInfo")]
 [assembly: AssemblyDescription("Cimbalino Windows Phone Toolkit")]
 [assembly: AssemblyConfiguration("")]
@@ -30,23 +27,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ee6e4bc4-766a-4e56-af11-5b6b03bf7583")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.2.2.0")]
 [assembly: AssemblyFileVersion("1.2.2.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
