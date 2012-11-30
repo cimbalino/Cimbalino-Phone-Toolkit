f53d93d46304dfdeb9cb1081599a31f49897ad15
Pedro Lamas <pedrolamas@gmail.com>
2012-11-30 16:03:54 +0000

    Added the IShareMediaService interface and implementation

---
+++ src/Cimbalino.Phone.Toolkit (WP71)/Services/IShareMediaService.cs
@@ -0,0 +1,29 @@
+﻿// ****************************************************************************
+// <copyright file="IShareMediaService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2012
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>30-12-2012</date>
+// <project>Cimbalino.Phone.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+namespace Cimbalino.Phone.Toolkit.Services
+{
+    /// <summary>
+    /// Represents a service capable of sharing media over the social networks configured on the device.
+    /// </summary>
+    public interface IShareMediaService
+    {
+        /// <summary>
+        /// Shows a dialog that enables the user to share media on the social networks of their choice.
+        /// </summary>
+        /// <param name="filePath">The path to the media file to share.</param>
+        void Show(string filePath);
+    }
+}
---
+++ src/Cimbalino.Phone.Toolkit (WP71)/Services/ShareMediaService.cs
@@ -0,0 +1,45 @@
+﻿// ****************************************************************************
+// <copyright file="ShareMediaService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2012
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>30-12-2012</date>
+// <project>Cimbalino.Phone.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+#if WP8
+using Microsoft.Phone.Tasks;
+#else
+using System;
+#endif
+
+namespace Cimbalino.Phone.Toolkit.Services
+{
+    /// <summary>
+    /// Represents an implementation of the <see cref="IShareMediaService"/>.
+    /// </summary>
+    public class ShareMediaService : IShareMediaService
+    {
+        /// <summary>
+        /// Shows a dialog that enables the user to share media on the social networks of their choice.
+        /// </summary>
+        /// <param name="filePath">The path to the media file to share.</param>
+        public void Show(string filePath)
+        {
+#if WP8
+            new ShareMediaTask()
+            {
+                FilePath = filePath
+            }.Show();
+#else
+            throw new NotImplementedException("This service is unavailable in WP7");
+#endif
+        }
+    }
+}
