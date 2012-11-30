68e203668ead26e24187021e592aecbcbc9000d7
Pedro Lamas <pedrolamas@gmail.com>
2012-11-30 16:11:32 +0000

    Added the IMarketplaceHubService interface and implementation

---
+++ src/Cimbalino.Phone.Toolkit (WP71)/Services/IMarketplaceHubService.cs
@@ -0,0 +1,28 @@
+﻿// ****************************************************************************
+// <copyright file="IMarketplaceHubService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2012
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>30-12-2012</date>
+// <project>Cimbalino.Phone.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+namespace Cimbalino.Phone.Toolkit.Services
+{
+    /// <summary>
+    /// Represents a service capable of showing the marketplace hub.
+    /// </summary>
+    public interface IMarketplaceHubService
+    {
+        /// <summary>
+        /// Shows the marketplace hub.
+        /// </summary>
+        void Show();
+    }
+}
---
+++ src/Cimbalino.Phone.Toolkit (WP71)/Services/MarketplaceHubService.cs
@@ -0,0 +1,33 @@
+﻿// ****************************************************************************
+// <copyright file="MarketplaceHubService.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2012
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>30-12-2012</date>
+// <project>Cimbalino.Phone.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using Microsoft.Phone.Tasks;
+
+namespace Cimbalino.Phone.Toolkit.Services
+{
+    /// <summary>
+    /// Represents a service capable of showing the marketplace hub.
+    /// </summary>
+    public class MarketplaceHubService : IMarketplaceHubService
+    {
+        /// <summary>
+        /// Shows the marketplace hub.
+        /// </summary>
+        public void Show()
+        {
+            new MarketplaceHubTask().Show();
+        }
+    }
+}
