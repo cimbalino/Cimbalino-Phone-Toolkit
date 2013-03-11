a7714cf90bbe9f45aa55e8d4b406a686cf06fd5f
Pedro Lamas <pedrolamas@gmail.com>
2013-03-11 11:37:50 +0000

    Added the MutexLock helper class

---
+++ src/Cimbalino.Phone.Toolkit (WP71)/Helpers/MutexLock.cs
@@ -0,0 +1,73 @@
+﻿// ****************************************************************************
+// <copyright file="MutexLock.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2013
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>11-03-2013</date>
+// <project>Cimbalino.Phone.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Threading;
+
+namespace Cimbalino.Phone.Toolkit.Helpers
+{
+    /// <summary>
+    /// Allows for inter-process locking and synchronization.
+    /// </summary>
+    public sealed class MutexLock
+    {
+        private readonly Mutex _mutex;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MutexLock"/> class.
+        /// </summary>
+        /// <param name="name">The name of the Mutex.</param>
+        public MutexLock(string name)
+        {
+            _mutex = new Mutex(false, name);
+        }
+
+        /// <summary>
+        /// Locks the Mutex and returns an <see cref="IDisposable"/> object that when disposed will release the Mutex.
+        /// </summary>
+        /// <returns>An <see cref="IDisposable"/> object that when disposed will release the Mutex.</returns>
+        public IDisposable Lock()
+        {
+#if WP8
+            try
+            {
+                _mutex.WaitOne();
+            }
+            catch (AbandonedMutexException)
+            {
+            }
+#else
+            _mutex.WaitOne();
+#endif
+
+            return new Releaser(this);
+        }
+
+        private class Releaser : IDisposable
+        {
+            private readonly MutexLock _mutexLock;
+
+            internal Releaser(MutexLock mutexLock)
+            {
+                _mutexLock = mutexLock;
+            }
+
+            public void Dispose()
+            {
+                _mutexLock._mutex.ReleaseMutex();
+            }
+        }
+    }
+}
