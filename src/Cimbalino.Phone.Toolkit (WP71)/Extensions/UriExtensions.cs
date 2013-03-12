a3807afde6ab32f50bc6b9fc7aa526815c18e626
Pedro Lamas <pedrolamas@gmail.com>
2013-03-12 11:44:21 +0000

    Added the UriExtensions class

---
+++ src/Cimbalino.Phone.Toolkit (WP71)/Extensions/UriExtensions.cs
@@ -0,0 +1,52 @@
+﻿// ****************************************************************************
+// <copyright file="UriExtensions.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2013
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>12-03-2013</date>
+// <project>Cimbalino.Phone.Toolkit</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cimbalino.Phone.Toolkit.Extensions
+{
+    /// <summary>
+    /// Provides a set of static (Shared in Visual Basic) methods for <see cref="Uri"/> instances.
+    /// </summary>
+    public static class UriExtensions
+    {
+        /// <summary>
+        /// Gets a collection of query string values.
+        /// </summary>
+        /// <param name="uri">The current uri.</param>
+        /// <returns>A collection that contains the query string values.</returns>
+        public static IDictionary<string, string> QueryString(this Uri uri)
+        {
+            if (!uri.IsAbsoluteUri || string.IsNullOrEmpty(uri.Query))
+            {
+                return new Dictionary<string, string>();
+            }
+
+            var query = uri.Query;
+
+            if (query.StartsWith("?"))
+            {
+                query = query.Substring(1);
+            }
+
+            return query.Split('&')
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(x => x.Split('='))
+                .ToDictionary(x => x[0], x => x.Length == 2 ? System.Net.HttpUtility.UrlDecode(x[1]) : null);
+        }
+    }
+}
