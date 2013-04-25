92fc67eac9278310fb90b3cb632ed33aca05af97
Pedro Lamas <pedrolamas@gmail.com>
2013-04-25 23:31:14 +0100

    Added the MultiBinding sample

---
+++ samples/MultiBinding/MultiBinding/ViewModel/MainViewModel.cs
@@ -0,0 +1,59 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace MultiBinding.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _text1;
+        private string _text2;
+
+        public string Text1
+        {
+            get
+            {
+                return _text1;
+            }
+            set
+            {
+                if (_text1 != value)
+                {
+                    _text1 = value;
+
+                    RaisePropertyChanged(() => Text1);
+                }
+            }
+        }
+
+        public string Text2
+        {
+            get
+            {
+                return _text2;
+            }
+            set
+            {
+                if (_text2 != value)
+                {
+                    _text2 = value;
+
+                    RaisePropertyChanged(() => Text2);
+                }
+            }
+        }
+
+        public string[] Items
+        {
+            get
+            {
+                return new[]
+                {
+                    "Both are empty",
+                    "Text 1 is empty",
+                    "Text 2 is empty",
+                    "None are empty",
+                };
+
+            }
+        }
+    }
+}
---
+++ samples/MultiBinding/MultiBinding/ViewModel/MultiStringsToStateConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using Cimbalino.Phone.Toolkit.Converters;
+
+namespace MultiBinding.ViewModel
+{
+    public class MultiStringsToStateConverter : MultiValueConverterBase<int>
+    {
+        public override int Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var text1 = (string)values[0];
+            var text2 = (string)values[1];
+
+            if (string.IsNullOrEmpty(text1))
+            {
+                if (string.IsNullOrEmpty(text2))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            else if (string.IsNullOrEmpty(text2))
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
+        }
+
+        public override object[] ConvertBack(int value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new object[]
+            {
+                value == -1 || value == 0 || value == 1 ? null : "This is not empty",
+                value == -1 || value == 0 || value == 2 ? null : "This is not empty"
+            };
+        }
+    }
+}
