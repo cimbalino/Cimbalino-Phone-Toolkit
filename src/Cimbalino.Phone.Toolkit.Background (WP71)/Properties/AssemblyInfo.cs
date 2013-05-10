8281df4ce48137a25d44d1f5bf0242dd5a2f0c17
Pedro Lamas <pedrolamas@gmail.com>
2013-05-10 11:24:35 +0100

    General refactoring on Cimbalino.Phone.Toolkit.Background project

--- src/Cimbalino.Phone.Toolkit.Background (WP71)/Properties/AssemblyInfo.cs
+++ src/Cimbalino.Phone.Toolkit.Background (WP71)/Properties/AssemblyInfo.cs
@@ -1,37 +1,38 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Resources;
+﻿// ****************************************************************************
+// <copyright file="AssemblyInfo.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2013
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>09-05-2013</date>
+// <project>Cimbalino.Phone.Toolkit.Camera</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
 [assembly: AssemblyTitle("Cimbalino.Phone.Toolkit.Background")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cimbalino Windows Phone Toolkit")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cimbalino.Phone.Toolkit.Background")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Pedro Lamas")]
+[assembly: AssemblyProduct("Cimbalino.Phone.Toolkit")]
+[assembly: AssemblyCopyright("Copyright © Pedro Lamas 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("960e9331-9264-4485-add3-c68966685311")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+[assembly: CLSCompliant(true)]
--- src/Cimbalino.Phone.Toolkit.Background (WP8)/Properties/AssemblyInfo.cs
+++ src/Cimbalino.Phone.Toolkit.Background (WP8)/Properties/AssemblyInfo.cs
@@ -1,37 +1,38 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Resources;
+﻿// ****************************************************************************
+// <copyright file="AssemblyInfo.cs" company="Pedro Lamas">
+// Copyright © Pedro Lamas 2013
+// </copyright>
+// ****************************************************************************
+// <author>Pedro Lamas</author>
+// <email>pedrolamas@gmail.com</email>
+// <date>09-05-2013</date>
+// <project>Cimbalino.Phone.Toolkit.Camera</project>
+// <web>http://www.pedrolamas.com</web>
+// <license>
+// See license.txt in this solution or http://www.pedrolamas.com/license_MIT.txt
+// </license>
+// ****************************************************************************
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Cimbalino.Phone.Toolkit.Background__WP8_")]
-[assembly: AssemblyDescription("")]
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cimbalino.Phone.Toolkit.Background")]
+[assembly: AssemblyDescription("Cimbalino Windows Phone Toolkit")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Cimbalino.Phone.Toolkit.Background__WP8_")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Pedro Lamas")]
+[assembly: AssemblyProduct("Cimbalino.Phone.Toolkit")]
+[assembly: AssemblyCopyright("Copyright © Pedro Lamas 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("93da53cf-e4be-43f1-904b-f0f7933a0af6")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
+
+[assembly: CLSCompliant(true)]
