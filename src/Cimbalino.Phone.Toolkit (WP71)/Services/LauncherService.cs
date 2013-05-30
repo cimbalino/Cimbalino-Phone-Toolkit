dce5d6d2bb7365d8336246a894aabb848c2c8f14
Pedro Lamas <pedrolamas@gmail.com>
2013-05-30 12:06:17 +0100

    LauncherService in WP7 will now try to use some of the known uris from WP8

--- src/Cimbalino.Phone.Toolkit (WP71)/Services/LauncherService.cs
+++ src/Cimbalino.Phone.Toolkit (WP71)/Services/LauncherService.cs
@@ -15,6 +15,8 @@
 
 using System;
 using System.Threading.Tasks;
+using Cimbalino.Phone.Toolkit.Extensions;
+using Microsoft.Phone.Tasks;
 #if WP8
 using Windows.Storage;
 using Windows.System;
@@ -38,9 +40,129 @@ namespace Cimbalino.Phone.Toolkit.Services
             return Launcher.LaunchUriAsync(uri)
                 .AsTask();
 #else
-            throw new NotSupportedException("This service is not supported in Windows Phone 7.x");
+            if (uri.Scheme == "http")
+            {
+                return ShowWebBrowserAsync(uri);
+            }
+
+            if (uri.Scheme == "mailto")
+            {
+                return ShowEmailComposeAsync(uri);
+            }
+
+            if (uri.Scheme == "zune")
+            {
+                switch (uri.LocalPath)
+                {
+                    case "navigate":
+                        return ShowMarketplaceDetailAsync(uri);
+
+                    case "reviewapp":
+                        return ShowMarketplaceReviewAsync(uri);
+
+                    case "search":
+                        return ShowMarketplaceSearchAsync(uri);
+                }
+            }
+
+            switch (uri.Scheme)
+            {
+                case "ms-settings-airplanemode":
+                    return ShowConnectionSettingsAsync(ConnectionSettingsType.AirplaneMode);
+
+                case "ms-settings-bluetooth":
+                    return ShowConnectionSettingsAsync(ConnectionSettingsType.Bluetooth);
+
+                case "ms-settings-cellular":
+                    return ShowConnectionSettingsAsync(ConnectionSettingsType.Cellular);
+
+                case "ms-settings-wifi":
+                    return ShowConnectionSettingsAsync(ConnectionSettingsType.WiFi);
+            }
+
+            throw new NotSupportedException("The specified uri is not supported in Windows Phone 7.x");
 #endif
         }
+
+#if !WP8
+        private Task ShowWebBrowserAsync(Uri uri)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                new WebBrowserTask()
+                {
+                    Uri = uri
+                }.Show();
+            });
+        }
+
+        private Task ShowEmailComposeAsync(Uri uri)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var queryString = uri.QueryString();
+
+                new EmailComposeTask()
+                {
+                    To = string.IsNullOrEmpty(uri.UserInfo) ? queryString.GetValue("to") : uri.UserInfo + "@" + uri.Host,
+                    Subject = queryString.GetValue("subject"),
+                    Body = queryString.GetValue("body"),
+                    Cc = queryString.GetValue("cc"),
+                    Bcc = queryString.GetValue("bcc")
+                }.Show();
+            });
+        }
+
+        private Task ShowMarketplaceDetailAsync(Uri uri)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                new MarketplaceDetailTask()
+                {
+                    ContentIdentifier = uri.QueryString().GetValue("appid")
+                }.Show();
+            });
+        }
+
+        private Task ShowMarketplaceReviewAsync(Uri uri)
+        {
+            if (!string.IsNullOrEmpty(uri.Query))
+            {
+                throw new NotSupportedException("The specified uri is not supported in Windows Phone 7.x");
+            }
+
+            return Task.Factory.StartNew(() => new MarketplaceReviewTask().Show());
+        }
+
+        private Task ShowMarketplaceSearchAsync(Uri uri)
+        {
+            var queryString = uri.QueryString();
+
+            if (!queryString.ContainsKey("keyword"))
+            {
+                throw new NotSupportedException("The specified uri is not supported in Windows Phone 7.x");
+            }
+
+            return Task.Factory.StartNew(() =>
+            {
+                new MarketplaceSearchTask()
+                {
+                    SearchTerms = queryString["keyword"]
+                }.Show();
+            });
+        }
+
+        private Task ShowConnectionSettingsAsync(ConnectionSettingsType connectionSettingsType)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                new ConnectionSettingsTask()
+                {
+                    ConnectionSettingsType = connectionSettingsType
+                }.Show();
+            });
+        }
+#endif
 
         /// <summary>
         /// Starts the default app associated with the specified file.
